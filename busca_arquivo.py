41443e887ef11eda3e16db97cdb87984d1af0472
d4rk3n3ss <54445515+d4rk3n3ss@users.noreply.github.com>
2020-07-22 01:28:59 -0300

    Update busca_arquivo.py
    adicionando menu

--- busca_arquivo.py
+++ busca_arquivo.py
@@ -5,32 +5,35 @@ from tkinter import ttk
 root=Tk()
 
 class Funcoes():
-    def endereco(self):
+    def buscar(self):
         lista =os.listdir(self.entr_caminho.get())
         elemento =self.entr_busca.get()
         valida = (elemento in lista)
         indice = lista.index(elemento)
         if valida ==True:
             self.lb_mostra['text']=str(indice)+('  -  ')+(elemento)
-        elif valida==False:
-            self.lb_mostra['Arquivo nao esta no diretorio']
+            self.entr_caminho.delete(0,END)
+            self.entr_busca.delete(0,END)
+
+
 class Aplicacao(Funcoes):
     def __init__(self):
         self.root=root
         self.Tela()
         self.Frame()
         self.Botao()
+        self.Menus()
         root.mainloop()
     def Tela(self):
         self.root.title('Buscador')
-        self.root.configure(background='#87CEFA')
+        self.root.configure(background='#2E2E2E')
         self.root.geometry('500x300')
         self.root.resizable(False, False)
     def Frame(self):
-        self.Main=Frame(self.root,bg= '#87CEFA',highlightbackground='#759fe6', highlightthickness=1)
+        self.Main=Frame(self.root,bg= '#81BEF7',highlightbackground='#759fe6')
         self.Main.place(relx= 0.01, rely=0.01, relwidth= 0.98, relheight=0.98)
     def Botao(self):
-        self.Bt_Procura = Button(self.Main, text='Procurar',command=self.endereco)
+        self.Bt_Procura = Button(self.Main, text='Procurar',command=self.buscar)
         self.Bt_Procura.place(relx=0.87, rely=0.15)
 
         self.entr_caminho=Entry(self.Main)
@@ -41,4 +44,13 @@ class Aplicacao(Funcoes):
 
         self.entr_busca=Entry(self.Main)
         self.entr_busca.place(relx=0.55, rely=0.16,relwidth=0.30, relheight=0.08)
+    def Menus(self):
+        manubar = Menu(self.root)
+        self.root.config(menu=manubar)
+        filemenu = Menu(manubar)
+        def Quit(): self.root.destroy()
+        manubar.add_cascade(label='Opçoes', menu=filemenu)
+        filemenu.add_command(label='Sair', command=Quit)
+
+
 Aplicacao()
