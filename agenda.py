400b6bef1b1b17b348efdc7066f6959ed661363a
d4rk3n3ss <54445515+d4rk3n3ss@users.noreply.github.com>
2020-07-24 12:17:19 -0300

    Update agenda.py
    melhorando

--- agenda.py
+++ agenda.py
@@ -5,11 +5,11 @@ import sqlite3
 root=Tk()
 
 class Funçoes():
-    def Novo(self):
+    def Agendar(self):
         print('Novo')
-    def Procura(self):
+    def Tarefas(self):
         print('Procurar')
-    def Exibir(self):
+    def VerMes(self):
         print('Exibindo')
     def Notas(self):
         print('Atualizando')
@@ -30,14 +30,14 @@ class Aplicacao(Funçoes):
         self.Main=Frame(self.root,bg= '#87CEFA',highlightbackground='#759fe6', highlightthickness=1)
         self.Main.place(relx= 0.01, rely=0.01, relwidth= 0.98, relheight=0.98)
     def Widget(self):
-        self.Bt_Novo=Button(self.Main,text='Novo',command=self.Novo)
-        self.Bt_Novo.place(relx=0.01, rely=0.10)
+        self.Bt_Novo=Button(self.Main,text='AGENDAR',command=self.Agendar)
+        self.Bt_Novo.place(relx=0.35, rely=0.10,relwidth=0.30,relheight=0.10)
 
-        self.Bt_Procura = Button(self.Main, text='Procurar',command=self.Procura)
-        self.Bt_Procura.place(relx=0.01, rely=0.20)
+        self.Bt_Procura = Button(self.Main, text='MINHAS TAREFAS',command=self.Tarefas)
+        self.Bt_Procura.place(relx=0.35, rely=0.35,relwidth=0.30,relheight=0.10)
 
-        self.Bt_exibe=Button(self.Main, text='Contatos',command=self.Exibir)
-        self.Bt_exibe.place(relx=0.01, rely=0.30)
+        self.Bt_exibe=Button(self.Main, text='VER MES',command=self.VerMes)
+        self.Bt_exibe.place(relx=0.35, rely=0.60,relwidth=0.30,relheight=0.10)
     def Menus(self):
         manubar=Menu(self.root)
         self.root.config(menu=manubar)
